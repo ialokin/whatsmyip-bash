083ea5636baec4dcf501548a1dd0badcfb02c2ff
Ialokin <git@ialokin.no>
2025-01-18 16:07:02 +0100

    Added a new example to --help. New option, -l|--list that lists all the webservices. closes #3

--- whatsmyip.sh
+++ whatsmyip.sh
@@ -34,6 +34,9 @@ ipWebServer=(
   "https://api.seeip.org/"
   "https://ident.me/"
   "https://checkip.dedyn.io/"
+  "https://myip.dk/"
+  "https://myexternalip.com/raw"
+  "https://ipapi.co/ip/"
 )
 
 # Function to display help
@@ -48,10 +51,12 @@ Options:
   -6, --ipv6       Use IPv6
   -s, --server     Specify the URL to query for the IP address.
                    Make sure it ONLY replies with the IP address.
+  -l, --list       List available web services
 
 Examples:
   $(basename "$0") --ipv6
   $(basename "$0") -h
+  $(basename "$0") -s https://myip.dk/ -4
 EOF
 }
 
@@ -63,6 +68,31 @@ Author:  Nikolai Stensen (ialokin)
          https://ialokin.no/ Email: dev@ialokin.no
 License: GNU GPL v3.0 ( https://www.gnu.org/licenses/gpl-3.0.en.html )
 EOF
+}
+
+# Function to check for curl/wget
+function ipCheckBrowser() {
+  if [[ -x "$(which curl)" ]]; then
+    echo "curl -s"  # Return the curl command string
+  elif [[ -x "$(which wget)" ]]; then
+    echo "wget -O -q -"  # Return the wget command string
+  else
+    return 1  # Return an error if neither curl nor wget is found
+  fi
+}
+
+# Function to check the IP address and try to verify if it looks like a valid
+# IPv4 or IPv6 address.
+function ipCheckVer() {
+  # Check for IPv4
+  if [[ "$1" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    echo "4"  # IPv4
+  # Check for IPv6
+  elif [[ "$1" =~ ^([0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{1,4}$ ]]; then
+    echo "6"  # IPv6
+  else
+    echo "0"  # Unknown
+  fi
 }
 
 # Parse arguments
@@ -92,12 +122,20 @@ while [[ $# -gt 0 ]]; do
     ipServer="$2"
     shift 2
   ;;
-   -*)
+  -l|--list)
+    echo "Available web services:"
+    echo
+    for server in "${ipWebServer[@]}"; do
+      echo "- ${server}"
+    done
+    exit 0
+  ;;
+  -*)
     echo "Unknown option: $1" >&2
     echo "Use --help to see available options." >&2
     exit 1
   ;;
-   *)
+  *)
     echo "Unexpected argument: $1" >&2
     echo "Use --help to see available options." >&2
     exit 1
@@ -105,13 +143,10 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-# Make sure we have either curl or wget installed.
-if [[ -x "$(which curl)" ]]; then
-  ipBrowser="curl -s"
-elif [[ -x "$(which wget)" ]]; then
-  ipBrowser="wget -O -q -"
-else
-  echo "ERROR! This script needs either curl or wget!"
+# Select browser, or die if neither curl or wget is available.
+ipBrowser=$(ipCheckBrowser)
+if [[ -z "$ipBrowser" ]]; then
+  echo "Error: curl or wget is required to run this script." >&2
   exit 1
 fi
 
@@ -120,9 +155,26 @@ fi
 if [ -z "${ipServer}" ]; then
   ipServer=${ipWebServer[$RANDOM % ${#ipWebServer[@]}]}
 fi
-# Get your IP address from the server.
+
+# Get your IP address from the server and try to verify it.
 ipReceived=$($ipBrowser "$ipProt" "$ipServer")
-# Check if there is a newline at the end of the IP address.
+ipVersion=$(ipCheckVer "$ipReceived")
+
+# Verify that we got the correct IP. If we tried to get IPv4 and got IPv6
+# it could mean that we dont have IPv4 connectiviity or vice versa.
+# Verify the IP address
+if [[ "$ipVersion" == "4" && "$ipProt" == "-6" ]]; then
+  echo "ERROR! Asked for IPv6, but got IPv4. Is the IPv6 connection down?" >&2
+  exit 1
+elif [[ "$ipVersion" == "6" && "$ipProt" == "-4" ]]; then
+  echo "ERROR! Asked for IPv4, but got IPv6. Is the IPv4 connection down?" >&2
+  exit 1
+elif [[ "$ipVersion" == "0" ]]; then
+  echo "ERROR! You asked for IPv4/IPv6, but the web service replied with something else!" >&2
+  exit 1
+fi
+
+# When printing the IP, make sure we have a newline at the end.
 if [[ "${ipReceived: -1}" == $'\n' ]]; then
   echo -n "$ipReceived"
 else
